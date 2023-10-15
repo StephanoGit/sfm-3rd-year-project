483e68b1872549f27ca8ae77501104c74dd923e4
Radu-Stefan Popovici <radu.popovici-2@student.manchester.ac.uk>
2023-10-15 18:14:37 +0100

    setup opencv dependencies for vscode and build flow

---
+++ c++/build/CMakeFiles/sfm.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sfm.dir/main.cpp.o"
+  "CMakeFiles/sfm.dir/main.cpp.o.d"
+  "sfm"
+  "sfm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sfm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
